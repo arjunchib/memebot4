859c54ff86161735dbeec8f7a31eaf015cd32263
Arjun Chib <arjunpatrickchib@gmail.com>
2026-06-02 00:10:39 -0400

    Add backfill transcription task

---
+++ app/tasks/backfill_transcription_task.ts
@@ -0,0 +1,26 @@
+import { transcriptionService } from "../services/transcription_service";
+import { db } from "../../db/database";
+import { Meme, Transcription } from "../../db/schema";
+import { eq, isNull } from "drizzle-orm";
+
+export class BackfillTranscriptionTask {
+  async perform() {
+    const memes = await db
+      .select({ id: Meme.id, name: Meme.name })
+      .from(Meme)
+      .leftJoin(Transcription, eq(Meme.id, Transcription.memeId))
+      .where(isNull(Transcription.memeId))
+      .limit(10);
+
+    let i = memes.length;
+
+    for (const { id, name } of memes) {
+      const t0 = performance.now();
+      await transcriptionService.transcibe(id);
+      const t1 = performance.now();
+      console.log(
+        `[${i--}] Transcribed ${name} in ${((t1 - t0) / 1000).toFixed(3)}s`,
+      );
+    }
+  }
+}
--- scripts/task.ts
+++ scripts/task.ts
@@ -2,7 +2,7 @@ import { pascalCase, snakeCase } from "change-case";
 import { styleText } from "node:util";
 
 const args = Bun.argv.slice(2);
-const taskName = args[0];
+const taskName = args[0] + "_task";
 
 if (!taskName) throw new Error("Need to provide a task name");
 
@@ -12,4 +12,12 @@ const snakeName = snakeCase(taskName);
 const task = await import(`../app/tasks/${snakeName}`);
 
 console.log(styleText("cyanBright", `Running ${pascalName}`));
-new task[pascalName]().perform();
+const t0 = performance.now();
+await new task[pascalName]().perform();
+const t1 = performance.now();
+console.log(
+  styleText(
+    "cyanBright",
+    `Finished ${pascalName} in ${((t1 - t0) / 1000).toFixed(3)}s`,
+  ),
+);
