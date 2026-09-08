37922d1b18db0bb1e65245d914a3ce2f37e53b97
Arjun Chib <arjunpatrickchib@gmail.com>
2026-09-08 00:56:41 -0400

    Add llm script

---
+++ scripts/llm.ts
@@ -0,0 +1,97 @@
+import {
+  LMStudioClient,
+  tool,
+  Chat,
+  type LLMPredictionFragmentReasoningType,
+} from "@lmstudio/sdk";
+import { z } from "zod";
+import { sqliteReadonly } from "../db/database";
+import { createInterface } from "readline/promises";
+
+const client = new LMStudioClient();
+
+const multiplyTool = tool({
+  name: "multiply",
+  description: "Given two numbers a and b. Returns the product of them.",
+  parameters: { a: z.number(), b: z.number() },
+  implementation: ({ a, b }) => a * b,
+});
+
+const readonlySqliteTool = tool({
+  name: "readonlySqlite",
+  description: "Given a sql query run against a read replica of the db.",
+  parameters: { query: z.string() },
+  implementation: ({ query }) => {
+    const results = sqliteReadonly.query(query).all();
+
+    let csv = Object.keys(results[0] as any).join(",");
+    csv += "\n";
+    csv += results
+      .map((result: any) => Object.values(result).join(","))
+      .join("\n");
+
+    return csv;
+  },
+});
+
+const model = await client.llm.model("google/gemma-4-e2b");
+
+const RED = Bun.color("red", "ansi");
+const TEAL = Bun.color("teal", "ansi");
+const GRAY = Bun.color("gray", "ansi");
+const GREEN = Bun.color("green", "ansi");
+const YELLOW = Bun.color("yellow", "ansi");
+const BOLD = "\x1b[1m";
+const RESET = "\x1b[0m";
+
+let prevReasoningType: LLMPredictionFragmentReasoningType;
+
+const chat = Chat.empty();
+const rl = createInterface({ input: process.stdin, output: process.stdout });
+
+const file = Bun.file("schema.sql");
+const schema = await file.text();
+chat.append(
+  "system",
+  `Duration is stored in seconds. Loudness values are generated from ffmpeg's \`loudnorm\` command. Here is the schema for the db: ${schema}`,
+);
+
+while (true) {
+  const input = await rl.question(`${BOLD}${TEAL}You:${RESET} `);
+  // Append the user input to the chat
+  chat.append("user", input);
+  process.stdout.write(`${BOLD}${RED}Bot:${RESET} `);
+  await model.act(chat, [readonlySqliteTool], {
+    // When the model finish the entire message, push it to the chat
+    onMessage: (message) => {
+      chat.append(message);
+    },
+    onPredictionFragment: (fragment) => {
+      const { content, reasoningType } = fragment;
+      if (reasoningType !== prevReasoningType) {
+        switch (reasoningType) {
+          case "none":
+            process.stdout.write(`${RESET}\n${content}`);
+            break;
+          case "reasoning":
+            process.stdout.write(`${RESET}${GRAY}${content}`);
+            break;
+          case "reasoningStartTag":
+          case "reasoningEndTag":
+            process.stdout.write(`${RESET}${GREEN}${content}`);
+            break;
+        }
+        prevReasoningType = reasoningType;
+      } else {
+        process.stdout.write(content);
+      }
+    },
+    onRoundStart() {
+      process.stdout.write(`${GRAY}`);
+    },
+    onToolCallRequestArgumentFragmentGenerated: (fragment, callId, content) => {
+      process.stdout.write(`${RESET}${YELLOW}${content}`);
+    },
+  });
+  process.stdout.write("\n");
+}
